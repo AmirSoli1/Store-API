614c878f5d30bf7f95bfe25bf4ca4b63abf11627
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-23 22:08:49 +0200

    link database to the project

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleWare = require('./middleware/not-found');
+const errorHandlerMiddleWare = require('./middleware/error-handler');
+
+const connectDB = require('./db/connect');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleWare);
+
+const port = process.env.port || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.DATABASE_URL);
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
